9777215a90fe1bf99a74601b56d426b7ad97dd61
Osama Walid Hasan <osamawalid22@outlook.com>
2022-05-14 21:30:03 +0300

    first commit

---
+++ Task/Network/ApiService.swift
@@ -0,0 +1,8 @@
+//
+//  ApiService.swift
+//  Task
+//
+//  Created by Osama Walid on 14/05/2022.
+//
+
+import Foundation
---
+++ Task/Utility/EndPoints.swift
@@ -0,0 +1,8 @@
+//
+//  EndPoints.swift
+//  Task
+//
+//  Created by Osama Walid on 14/05/2022.
+//
+
+import Foundation
